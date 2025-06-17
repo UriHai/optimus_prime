5a5f4df9dbc65bd98c213cd5a935eff8d31b5a4e
uri <13288uri@gmail.com>
2025-06-17 16:53:58 +0300

    format fixes

--- optimus_prime/Prime.cpp
+++ optimus_prime/Prime.cpp
@@ -4,44 +4,35 @@ using std::cout;
 using std::endl;
 
 /**
- * Check wether a number is prime
+ * @brief Check wether a number is prime
  *
  * @param num	[IN]	The number to check
  * @return true if num is prime, false otherwise
  */
-bool isPrime(int num)
-{
-    int divisor = 2;
-    if (num <= 1)
-    {
+bool isPrime(unsigned int num) {
+    unsigned int divisor = 2;
+    if (num <= 1) {
         return false;
     }
 
-    for (divisor = 2; divisor < num / 2 + 1; divisor++)
-    {
-        if (num % divisor == 0)
-        {
+    for (divisor = 2; divisor < num / 2 + 1; divisor++) {
+        if (num % divisor == 0) {
             return false;
         }
     }
     return true;
 }
 
-int main()
-{
-    int i = 0;
+int main() {
+    unsigned int i = 0;
     bool isIPrime = false;
     cout << "Running isPrime numbers 0 - 10" << endl;
-    for (i = 0; i < 11; i++)
-    {
+    for (i = 0; i < 11; i++) {
         isIPrime = isPrime(i);
         cout << i << ": ";
-        if (isIPrime)
-        {
+        if (isIPrime) {
             cout << "Prime" << endl;
-        }
-        else
-        {
+        } else {
             cout << "Composite" << endl;
         }
     }

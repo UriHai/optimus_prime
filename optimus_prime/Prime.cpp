0481b2c8f5e6813ebfe3ab7badbd6b71cdca173b
uri <13288uri@gmail.com>
2025-06-17 16:07:46 +0300

    attempt two at commiting isPrime()

--- optimus_prime/Prime.cpp
+++ optimus_prime/Prime.cpp
@@ -5,34 +5,43 @@ using std::endl;
 
 /**
  * Check wether a number is prime
- * 
+ *
  * @param num	[IN]	The number to check
  * @return true if num is prime, false otherwise
  */
-bool isPrime(int num) {
+bool isPrime(int num)
+{
     int divisor = 2;
-    if (num <= 1) {
+    if (num <= 1)
+    {
         return false;
     }
 
-    for (divisor = 2; divisor < num / 2 + 1; divisor++) {
-        if (num % divisor == 0) {
+    for (divisor = 2; divisor < num / 2 + 1; divisor++)
+    {
+        if (num % divisor == 0)
+        {
             return false;
         }
     }
     return true;
 }
 
-int main() {
+int main()
+{
     int i = 0;
     bool isIPrime = false;
     cout << "Running isPrime numbers 0 - 10" << endl;
-    for (i = 0; i < 11; i++) {
+    for (i = 0; i < 11; i++)
+    {
         isIPrime = isPrime(i);
         cout << i << ": ";
-        if (isIPrime) {
+        if (isIPrime)
+        {
             cout << "Prime" << endl;
-        } else {
+        }
+        else
+        {
             cout << "Composite" << endl;
         }
     }
